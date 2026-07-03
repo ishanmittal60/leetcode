36758915b464340f886f6cc0b447770e3b88dd09
Ishan Mittal <151866166+ishanmittal60@users.noreply.github.com>
2026-07-03 16:42:04 +0530

    Time: 27 ms (69.48%), Space: 44.3 MB (32.55%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -11,7 +11,17 @@ int re(string s, string t,int i , int j, vector<vector<int>> &dp ){
         int n = s.size();
         int m = t.size();
        // reverse(t.begin(),t.end());
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-       return re(s,t,n-1,m-1,dp);
+    //     vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+    //    return re(s,t,n-1,m-1,dp);
+     vector<vector<unsigned long long>>dp(n+1,vector<unsigned long long>(m+1,0));
+     for(int i = 0; i <= n; i++)
+    dp[i][0] = 1;
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s[i-1]==t[j-1])  dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+   else dp[i][j]= dp[i-1][j];
+        }
+     }
+     return dp[n][m];
     }
 };
